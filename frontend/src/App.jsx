84021e07bbb0e1ff74ef3fbf9b8f3f746e594429
yash-0025 <yashpatel200403@gmail.com>
2025-04-25 02:25:25 +0530

    Basic Project Setup :: Frontend, Backend Setuup Done ✅

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className='text-black text-center font-extrabold text-7xl mt-3 sm:text-amber-300'>
+      Task Manager
+      </h1>
+    </>
+  )
+}
+
+export default App

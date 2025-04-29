5b03508dcb6bb503e49cee4a23c1c10c433729b1
yash-0025 <yashpatel200403@gmail.com>
2025-04-30 01:57:57 +0530

    Api Updates

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: "/api",
-    // baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api"
-    baseURL: "http://localhost:5000/api",
+
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api"
+    // baseURL: "http://localhost:5000/api",
 });
 
 export default api;
